fde21efaf3810cfb1f3d8a86051d63e62858c14b
Kayo Vinicius <kayovinicius.dev@gmail.com>
2024-10-24 17:13:10 -0300

    adicionar item na lista com js

---
+++ scripts.js
@@ -0,0 +1,37 @@
+const item = document.getElementById("input-item");
+const botaoSalvarItem = document.getElementById("adicionar-item");
+const listaDeCompras = document.getElementById("lista-de-compras");
+
+botaoSalvarItem.addEventListener("click", adicionarItem);
+
+function adicionarItem(evento) {
+    evento.preventDefault();
+    
+    const itemDaLista = document.createElement("li");
+    const containerItemLista = document.createElement("div");
+    containerItemLista.classList.add("item-lista-container");
+
+    const conatainerNomeDoItem = document.createElement("div");
+    const nomeDoItem = document.createElement("p");
+    nomeDoItem.innerText = item.value;
+    conatainerNomeDoItem.appendChild(nomeDoItem);
+
+    const containerBotoes = document.createElement("div");
+    const botaoRemover = document.createElement("button")
+    botaoRemover.classList.add("botao-acao");
+
+    const imagemRemover = document.createElement("img");
+    imagemRemover.src = "img/delete.svg";
+    imagemRemover.alt = "Remover";
+
+    botaoRemover.appendChild(imagemRemover);
+    containerBotoes.appendChild(botaoRemover);
+
+    //fazer o botão editar
+
+    containerItemLista.appendChild(conatainerNomeDoItem);
+    containerItemLista.appendChild(containerBotoes);
+    itemDaLista.appendChild(containerItemLista);
+    listaDeCompras.appendChild(itemDaLista);
+
+}
