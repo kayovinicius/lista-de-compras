ec76f7f4e9f3ac2b8261ce05d430001ebe0fccab
Kayo Vinicius <kayovinicius.dev@gmail.com>
2024-10-30 19:25:16 -0300

    checked ao clicar no checkbox

--- scripts.js
+++ scripts.js
@@ -28,6 +28,17 @@ function adicionarItem(evento) {
     const checkboxLabel = document.createElement("label");
     checkboxLabel.setAttribute("for", checkboxInput.id);
 
+    checkboxLabel.addEventListener("click", function (evento) {
+        const checkboxInput = evento.currentTarget.querySelector(".checkbox-input");
+        const checkboxCustomizado = evento.currentTarget.querySelector(".checkbox-customizado");
+
+        if (checkboxInput.checked) {
+            checkboxCustomizado.classList.add("checked");
+        } else {
+            checkboxCustomizado.classList.remove("checked");            
+        }
+    })
+
     const checkboxCustomizado = document.createElement("div");
     checkboxCustomizado.classList.add("checkbox-customizado");
 
